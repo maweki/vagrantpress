c16adc13b28d159231478a3e1f36b89c5ece96bf
Mario Wenzel <maweki@gmail.com>
2015-06-30 10:45:20 +0200

    Disabled debug mode
    we don't develop plugins

--- puppet/modules/wordpress/files/wp-config.php
+++ puppet/modules/wordpress/files/wp-config.php
@@ -78,7 +78,7 @@ define('WPLANG', 'de_DE');
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
 
